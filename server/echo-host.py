03115d34e09282062eed24208e1be905d70d1944
Al0102 <mealex2007@gmail.com>
2026-01-25 14:04:55 -0800

    :new:Initial commit
    Template server and client with socket

---
+++ server/echo-host.py
@@ -0,0 +1,31 @@
+import socket
+
+HOST_IP = "127.0.0.1"
+PORT = 65432
+
+# Bind socket with corresponding address type and socket tupe (TCP)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+    # Connect specific network interface port
+    server.bind((HOST_IP, PORT))
+
+    # Enables incoming connections
+    server.listen()
+    print(f"Listening on {HOST_IP}:{PORT}...")
+
+    # Block until a client connects
+    connection, address = server.accept()
+
+    with connection:
+        print(f"Connected at {address}")
+        while True:
+            # Recieve client data (bytes)
+            data = connection.recv(1024)
+
+            # Empty bytes object siginifying client disconnect
+            if not data:
+                break
+
+            # Send data to client (bytes)
+            connection.sendall(data)
+
+print("Server stopped.")
