d559acaa4674ca4a931165ce618cba1a79e28c99
Al0102 <alu59@my.bcit.ca>
2026-06-03 19:29:18 -0700

    Add quit game option to Root

--- src/game/root.py
+++ src/game/root.py
@@ -9,14 +9,20 @@ from game.settings import Settings
 
 class Root(metaclass=Singleton):
     def __init__(self):
-        self.scene_switch_signal = Signal(SceneSwitchType, SCENE)
         self.settings = Settings()
+
+        self._init_signals()
 
     def switch_scene(
         self,
         switch_type: SceneSwitchType = SceneSwitchType.REPLACE,
         scene: SCENE = None
-    ):
+    ) -> None:
         self.scene_switch_signal.emit(switch_type, scene)
 
+    def quit(self, status: int=0) -> None:
+        self.quit_game_signal.emit(status)
 
+    def _init_signals(self):
+        self.scene_switch_signal = Signal(SceneSwitchType, SCENE)
+        self.quit_game_signal = Signal(int)
